651c115a9c53f55725e2dacc1ffc28e2a042b3fe
Jaime Daza <jaimedaza11@gmail.com>
2020-04-29 20:00:27 -0500

    remove class

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Mi primera aplicación</h1>
       <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
